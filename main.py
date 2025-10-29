1d44c6ddbb1615224560a7e718409d6310610e89
zdyre <103067571+zdyre@users.noreply.github.com>
2025-10-28 18:07:42 -0600

    Refactor main.py with structured sections and comments
    Refactor main.py to improve organization and readability by adding section headers and comments.

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
-## Class and function definitions in external files
+### 0 ### SETUP
+##########################################################
+## 0.1 ## Class and function definitions in external files
+
 import cv2
 import numpy as np
 from node import Node
 import velocityFunc as vf
-import track as tr
 import time
 import math
 
-## Read initial parameters/code settings
+##############################################################
+## 0.2 ## Read initial parameters/code settings - USER DEFINED
 
 # video source (camera index or file path, 0 = default camera)
 videoIn = 0
@@ -24,17 +27,19 @@ dilIter = 2
 # min contour area (square pixels)
 minArea = 500
 
-## Establish camera connection and initialize out files
+### 1 ### Initialization
+#####################################
+## 1.1 ## Establish camera connection 
 capture = cv2.VideoCapture(videoIn)
 success, _ = capture.read()
 if not success: # check if video source successfully capturing
     print("No Video Source found. \n")
     quit(0)
 
-# Create background subtractor [https://docs.opencv.org/3.4/d7/d7b/classcv_1_1BackgroundSubtractorMOG2.html]
-fgbg = cv2.createBackgroundSubtractorMOG2()
+#################################
+## 1.2 ## Initialize output files 
 
-# Define frame dimensions and output file
+# Define frame dimensions and video output files
 frame_W = int(capture.get(3))
 frame_H = int(capture.get(4))
 outraw_mp4 = cv2.VideoWriter('rawCap.mp4', cv2.VideoWriter_fourcc(*'mp4v'), 30, (frame_W,frame_H))
@@ -49,26 +54,49 @@ rawData.write("id, x, y, t,\n")
 velData = open('velocityDataOut.txt', 'w')
 velData.write("id, vx, vy, v, theta,\n")
 
+###################################################
+## 1.3 ## Initialize recording & tracking variables
+
+# bool to determine when saving data vs not
 recording = False # not recording by default
+
+# stores current recorded frame num, for node data
 recFrameCount = 0 # current frame count of recorded frames
-idCount = 0 # current number of object IDs (tracked objects)
+
+# current number of object IDs (tracked objects)
+idCount = 0 
+
+# bool determining if info display is shown
 info = False # info display off by default
-allNodes = [] # empty array to store all Nodes (data not saved)
-recNodes = [] # empty array to store recorded Nodes
-roiArr = [] # empty region-of-interest array
+
+# empty array to store all Nodes (data not saved) for info display
+allNodes = [] 
+
+# empty array to store recorded Nodes
+recNodes = [] 
+
+# index to choose displayed frame (to user) using keys 1-7
 dispIndex = -1 # tracking frame by default
 
-## Enter loop to display camera feed
+# Create background subtractor [https://docs.opencv.org/3.4/d7/d7b/classcv_1_1BackgroundSubtractorMOG2.html]
+fgbg = cv2.createBackgroundSubtractorMOG2()
+
+### 2 ### Extract data from frames (openCV)
+####################################################
+## 2.1 ## Enter loop to display & record camera feed
+
 while True:
     # Grab frame from video capture
     success, frame = capture.read()
     if not success: # check if successful
         break
-    # add different keys to change window view (optional)
 
-    track = frame.copy()
+    # separate video frame-stream saved to outproc_mp4 file, draws tracking boxes on video
+    track = frame.copy() 
 
-    ## Apply image filters to find object
+    #####################################################
+    ## 2.2 ## Apply image filters to find object contours
+
     # Blur to decrease noise [https://docs.opencv.org/4.x/d4/d13/tutorial_py_filtering.html]
     blur = cv2.medianBlur(frame, blurSize)
 
@@ -83,35 +111,39 @@ while True:
 
     # Find contours [https://docs.opencv.org/3.4/d4/d73/tutorial_py_contours_begin.html]
     contours, _ = cv2.findContours(dilated, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+    # create frame with contours drawn as a display option (keys 1-7)
     cont_frame = cv2.drawContours(frame.copy(), contours,  -1, (0,255,0), 3)
 
+    ### Discretize objects
+    #########################################################
+    ## 3.1 ## Track objects across frames
+
+    # retrieve nodes from prior objects for 'id' tracking
     prevNodes = Node.getPrevNodes(recNodes)
 
     # Loop through possible objects and draw rectangles
     for contour in contours:
-        if cv2.contourArea(contour) < minArea: # Filter small contours (square pixel area)
+
+        # Filter out small contours (square pixel area)
+        if cv2.contourArea(contour) < minArea: 
             continue
 
         # determine bounding rectangle dimensions
         (x, y, w, h) = cv2.boundingRect(contour)
-
-        # draw rectangle
-        cv2.rectangle(track, (x, y), (x + w, y + h), (0, 255, 0), 2)
 
         # determine centroid & current time
         center_x = x + w//2
         center_y = y + h//2
         curr_time = time.time()
 
-        allNodes.append(Node(center_x, center_y, curr_time, -1))
+        #############################################
+        ## 3.2 ## Populate node position and ROI data
 
         # check if recording to save specific frame data
         if recording == True:
             # update frame count
             recFrameCount = recFrameCount + 1
-
-            # for traced path image, mark centroid dot
-            cv2.circle(traceImg, (center_x, center_y), 1, (0, 0, 255), -1)
 
             # velocity calculation data 
             recNode = Node(center_x, center_y, curr_time, recFrameCount)
@@ -119,18 +151,41 @@ while True:
             # set new region of interest and update idCount
             idCount = recNode.trackID(prevNodes, idCount, (frame_W, frame_H))
 
+            # save to array
             recNodes.append(recNode)
+
+            ### 4 ### Live data logging
+            ###########################
+            ## 4.1 ## Save raw node data to file
+
+            # for traced-path image, mark centroid dot 
+            cv2.circle(traceImg, (center_x, center_y), 1, (0, 0, 255), -1)
 
             # save raw data to file
             rawData.write(f"{recNode.id}, {center_x}, {center_y}, {curr_time},\n")
 
+        ## 4.2 ## Save frame data
 
-    ## Check if recording for file output
+        # draw rectangle to track frame 
+        cv2.rectangle(track, (x, y), (x + w, y + h), (0, 255, 0), 2)
+
+        # save data to allNodes, distinguish from recorded data by frame_num = -1 
+        allNodes.append(Node(center_x, center_y, curr_time, -1))
+
+
+
+
+    ## 4.3 ## Check if recording for video file output
     if recording == True:
 
         # save tracked object data to video file
         outraw_mp4.write(frame)
         outproc_mp4.write(track)
+
+    ### 5 ### Display window (live)
+
+    ###################################################
+    ## 5.1 ## set display frame & recording dot overlay
 
     # initialize array of video streams (for 1-7 key functionality)
     dispArr = [frame, blur, fgmask, thresh, dilated, cont_frame, track]
@@ -140,6 +195,8 @@ while True:
     if recording == True: 
         cv2.circle(disp, (50, 50), 15, (0, 0, 255), -1)
     
+    #############################
+    ## 5.2 ## display info screen
     if info == True:
         cv2.putText(disp, "I", (frame_W-70, 70),cv2.FONT_HERSHEY_COMPLEX,2,(240,180,50),2)
         if len(allNodes) > 2: # check if at startup (empty array)
@@ -154,11 +211,14 @@ while True:
             cv2.putText(disp, f"Tot V : {math.floor(vi[0])}", (20,frame_H-20),cv2.FONT_HERSHEY_PLAIN,1.2,(240,180,50),2)
             cv2.putText(disp, f"Theta : {math.floor(thetai[0])}", (160,frame_H-20),cv2.FONT_HERSHEY_PLAIN,1.2,(240,180,50),2)
 
-
-    ## Display video feed
+    # Display video feed
     cv2.imshow('Display', disp)
 
-    ## User key input processing
+
+    ### 6 ### User key input processing
+    #####################################################
+    ## 6.1 ## set frame, toggle overlays, or exit program
+
     waitKey = cv2.waitKey(10) & 0xFF #delay in ms between checks
     if waitKey == ord('q'): # quit
         break
@@ -188,14 +248,23 @@ outproc_mp4.release()
 cv2.destroyAllWindows()
 
 
-## Output 
-# image connecting position dots
+### 7 ### Post-processing data logging 
+#######################################
+## 7.1 ## traced image display and file
+
+# display img
 cv2.imshow("Traced Path", traceImg)
 while cv2.waitKey(1) != ord('q'):
     pass
+
+# save traced image to file
 cv2.imwrite('TracedImg.png', traceImg)
 
+# close window
 cv2.destroyAllWindows()
+
+#################################
+## 7.2 ## Terminal printed output
 
 # print velocity data to file and terminal
 for j in range(idCount):
@@ -221,5 +290,3 @@ for j in range(idCount):
 # release data files
 rawData.close()
 velData.close()
-
-# TBD
