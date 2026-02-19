ad2c496a71f194e32267471f0cddcb20e3fb1781
zdyre <103067571+zdyre@users.noreply.github.com>
2026-02-19 12:51:58 -0700

    Update main
    Update main file to current implementation

--- newBuild/main.py
+++ newBuild/main.py
@@ -21,19 +21,55 @@
 
 import cv2
 import numpy as np
+import glob
 import time
 import math
 
-def initCam(source):
+def initCam(source): # connect video stream and calibrate lens distortion
     capture = cv2.VideoCapture(source)
 
     # test video stream connection
     success, _ = capture.read()
     if not success:
         print(f"unable to connect to video stream at source:{source}")
-        return 0, capture
+        return 0, capture, None, None
+        
+    # lens calibration code
     
-    return 1, capture
+    #termination criteria
+    criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
+        
+    # prepare object points
+    objp = np.zeros((25*25,3), np.float32)
+    objp[:,:2] = np.mgrid[0:25, 0:25].T.reshape(-1,2)
+    
+    # arrays to store points and image points from all images
+    objPoints = [] # 3D points in real-world space
+    imgPoints = [] # 2D points in image plane
+    
+    images = glob.glob('photos/*.png')
+    shape = None
+    for fname in images:
+        img = cv2.imread(fname)
+        #print(fname)
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        
+        shape = gray.shape[::-1]
+        
+        #find chessboard corners
+        ret, corners = cv2.findChessboardCorners(gray, (25,25), None)
+        
+        if ret == True:
+            #print("check")
+            
+            objPoints.append(objp)
+            
+            corners2 = cv2.cornerSubPix(gray, corners, (11,11), (-1,-1), criteria)
+            imgPoints.append(corners2)
+    
+    ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objPoints, imgPoints, shape, None, None)
+    
+    return 1, capture, mtx, dist
 
 def filterRGB(color_idx, frame, minThresh, maxDiff):
     # Create a mask to filter out pixels where the target color is too dim
@@ -63,9 +99,11 @@ def filterRGB(color_idx, frame, minThresh, maxDiff):
 
     # Apply Binary threshold at specified intensity (0-255) [https://docs.opencv.org/4.x/d7/d4d/tutorial_py_thresholding.html]
     _, filtered = cv2.threshold(filtered, 1, 255, cv2.THRESH_BINARY)
+    
+    # invert black and white for contouring
+    #filtered = cv2.bitwise_not(filtered)
             
     return filtered
-
 
 def xyzTransform(x, y, pxSize, trueSize, focalLength, frameDim):
     fx = frameDim[0] * focalLength
@@ -77,24 +115,20 @@ def xyzTransform(x, y, pxSize, trueSize, focalLength, frameDim):
 
     return x, y, z
 
-def correctLensDistortion(x, y, z):
-    # future implementation needed, use openCV calibration to setup
-    return x, y, z
-
 # camera dimensions [m]
 # option 1:
 #lensWidth = 0.03
 #sensorWidth = 0.008
 #focalLength = lensWidth/sensorWidth    
 # option 2:   
-fieldOfView = 60 # in degrees
+fieldOfView = 53 # in degrees, adjust for the cropped view after removing distortion
 focalLength = 0.5 / (math.tan(fieldOfView * math.pi / 360))
 
-# object real-world size [m]
+# object real-world size [m]121
 targetObjSize = 0.08
 
 # minimum object size detectable in pixel area
-minObjSize = 200            
+minObjSize = 10            
 
 # Camera source (file or index)
 videoIn = 0
@@ -103,8 +137,8 @@ videoIn = 0
 currFrameNodes = []
 prevFrameNodes = []
 
-# Camera initialization
-success, capture = initCam(videoIn)
+# Camera initialization (calibrate distortion)
+success, capture, mtx, dist = initCam(videoIn)
 
 if not success:
     quit(0)
@@ -122,14 +156,18 @@ while True:
     if not success: # check if successful
         break
 
-    filtered = filterRGB(2, frame, 50, 50) 
+    ncmtx, roi = cv2.getOptimalNewCameraMatrix(mtx, dist, (frame_W,frame_H), 0, (frame_W,frame_H))
+    #mapx, mapy = cv2.initUndistortRectifyMap(mtx, dist, None, ncmtx, (frame_W,frame_H), 5)
+    #undistorted = cv2.remap(filtered, mapx, mapy, cv2.INTER_LINEAR)
+    undistorted = cv2.undistort(frame, mtx, dist, None, ncmtx)
+    
+    cropped = undistorted[roi[1]:roi[1]+roi[3], roi[0]:roi[0]+roi[2]]
+    
+    filtered = filterRGB(2, cropped, 50, 20) 
 
-    dilated = cv2.dilate(filtered, None, iterations=2)
+    contours, _ = cv2.findContours(filtered, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
-    contours, _ = cv2.findContours(dilated, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-
-    cont_frame = frame.copy()
-    cont_dilated = dilated.copy()
+    cont_frame = cropped.copy()
 
     # loop through all objects in current frame
     prevFrameNodes = currFrameNodes
@@ -141,7 +179,6 @@ while True:
             continue
 
         cont_frame = cv2.drawContours(cont_frame, contour,  -1, (0,255,0), 3)
-        cont_dilated = cv2.drawContours(cont_dilated, contour,  -1, (0,255,0), 3)
 
         # determine bounding rectangle dimensions
         (x, y, w, h) = cv2.boundingRect(contour)
@@ -155,14 +192,14 @@ while True:
         
         # estimate object position in cartesian coordinates
         (x, y, z) = xyzTransform(c_x, c_y, unitLength, targetObjSize, focalLength, (frame_W, frame_H))
-        (x, y, z) = correctLensDistortion(x, y, z)
 
         # save object position
         obj = [x, y, z, currTime]
+        print(obj)
         currFrameNodes.append(obj)
 
     # initialize array of video streams (for 1-7 key functionality)
-    dispArr = [frame, filtered, dilated, cont_frame, cont_dilated]
+    dispArr = [frame, undistorted, cropped, filtered, cont_frame]
     disp = dispArr[dispIndex].copy()
 
     # write to file
@@ -179,11 +216,11 @@ while True:
         dispIndex = 0
     elif waitKey== ord('2'): # view filtered frame
         dispIndex = 1
-    elif waitKey== ord('3'): # view dilted frame
+    elif waitKey== ord('3'): # view undistorted frame
         dispIndex = 2
-    elif waitKey== ord('4'): # view contours on raw frame
+    elif waitKey== ord('4'): # view cropped frame
         dispIndex = 3
-    elif waitKey== ord('5'): # view contours on dilated frame
+    elif waitKey== ord('5'): # view contours on frame
         dispIndex = 4
 
 
@@ -191,11 +228,3 @@ capture.release()
 raw_mp4.release()
 contour_mp4.release()
 cv2.destroyAllWindows()
-    
-
-
-
-
-
-
-    
